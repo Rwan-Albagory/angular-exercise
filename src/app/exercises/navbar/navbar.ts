d7e5c594c65dea0e19fde39ddfdd56a9e7bb621b
Youssef Ezzat <ezzatyoussef79@gmail.com>
2026-09-04 22:04:15 +0300

    style: update UI with new styles

--- src/app/exercises/navbar/navbar.ts
+++ src/app/exercises/navbar/navbar.ts
@@ -1,9 +1,11 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import {
+  NavigationEnd,
   Router,
   RouterLink,
   RouterLinkActive
 } from '@angular/router';
+import { filter } from 'rxjs';
 
 import { CartService } from '../../core/core/services/cart.service';
 
@@ -17,9 +19,19 @@ import { CartService } from '../../core/core/services/cart.service';
 export class Navbar {
   private router = inject(Router);
   cartService = inject(CartService);
+  menuOpen = signal(false);
+
+  constructor() {
+    this.router.events
+      .pipe(filter((event): event is NavigationEnd => event instanceof NavigationEnd))
+      .subscribe(() => this.menuOpen.set(false));
+  }
+
   get isHome(): boolean {
     return this.router.url === '/' || this.router.url.startsWith('/?');
   }
+
+  toggleMenu(): void {
+    this.menuOpen.update((open) => !open);
+  }
 }
-
-
