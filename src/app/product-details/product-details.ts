f0cc854263f658adb240fc9319c0b640d3bbf06e
Rwan <rwanalbagory@gmail.com>
2026-09-11 17:47:28 +0300

    feat : update UI

---
+++ src/app/product-details/product-details.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  selector: 'app-product-details',
+  templateUrl: './product-details.html',
+})
+export class ProductDetails {}
