e2302e9af664341c44ec64cd474c0e21ff61263b
Rwan Albagory <rwanalbagory@gmail.com>
2026-09-15 12:52:42 +0300

    feat : updates

--- src/app/core/core/services/cart.service.ts
+++ src/app/core/core/services/cart.service.ts
@@ -11,21 +11,18 @@ export class CartService {
 cartItems = signal<ProductWithQuantity[]>([]);
 cartCount = computed(() => this.cartItems().reduce( (sum, product) => sum + product.quantity, 0 ) );
 
-//when user add 0 product 
+
 addToCart(product: ProductWithQuantity): void {
 if (product.quantity === 0) {
     return;
 }
-// Check if the product already exists in the cart and update its quantity if it does, otherwise add it to the cart 
 const existingProduct = this.cartItems().find(
     (item) => item.id === product.id
 );
 
-// Update the quantity of the existing product or add the new product to the cart
 if (existingProduct) {
     this.cartItems.update((items) =>
     items.map((item) =>
-    //Ternary operator to check if the item id matches the product id, if it does, update the product, otherwise return the item as is
     item.id === product.id
         ? { ...product }
         : item
@@ -33,9 +30,7 @@ if (existingProduct) {
     );
 
 } else {
-// Add the new product to the cart
     this.cartItems.update((items) => [
-    // Spread operator ?????
     ...items,
     { ...product }
     ]);
@@ -49,7 +44,6 @@ getQuantity(productId: number): number {
 const product = this.cartItems().find(
     (item) => item.id === productId
 );
-//if the product is found, return its quantity, otherwise return 0
 return product?.quantity ?? 0;
 }
 removeFromCart(productId: number): void {
@@ -58,4 +52,13 @@ this.cartItems.update((items) =>
     items.filter((item) => item.id !== productId)
 );
 }
+updateQuantity(productId: number, quantity: number): void {
+    this.cartItems.update((items) =>
+      items.map((item) =>
+        item.id === productId
+          ? { ...item, quantity }
+          : item
+      )
+    );
+  }
 }
--- src/app/exercises/cart/cart.ts
+++ src/app/exercises/cart/cart.ts
@@ -1,4 +1,6 @@
 import { Component, inject } from '@angular/core';
+import { CartService } from '../../core/core/services/cart.service';
+import { ProductWithQuantity } from '../../core/core/models/product.model';
 
 @Component({
   standalone: true,
@@ -7,5 +9,41 @@ import { Component, inject } from '@angular/core';
   templateUrl: './cart.html',
 })
 export class Cart {
+  cartService = inject(CartService);
 
+  increase(product: ProductWithQuantity): void {
+    if (product.quantity < 10) {
+      this.cartService.updateQuantity(product.id, product.quantity + 1);
+    }
+  }
+
+  decrease(product: ProductWithQuantity): void {
+    if (product.quantity > 1) {
+      this.cartService.updateQuantity(product.id, product.quantity - 1);
+    }
+  }
+
+  getSubtotal(): number {
+    return this.cartService.cartItems().reduce(
+      (total, product) =>
+        total + product.price * product.quantity,
+      0
+    );
+  }
+
+  getDiscount(): number {
+    return this.cartService.cartItems().reduce(
+      (total, product) =>
+        total +
+        (product.price *
+          product.quantity *
+          product.discountPercentage) /
+          100,
+      0
+    );
+  }
+
+  getNetTotal(): number {
+    return this.getSubtotal() - this.getDiscount();
+  }
 }
